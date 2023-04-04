3763a2bd77d2151eb020378ec6df34155ef62925
ddt_sekiguchi <a@abc.com>
2023-04-04 18:40:55 +0900

    1.　クラスヘッダにコメントを記載してください。 2. 　関数ヘッダにコメントを記載してください。 3. 　1処理ごとにコメントを記載してください(何をしているかが分かるような)。 3つの指摘がございましたので、修正致しました。 ご確認お願いします。

--- ensyu1_1/src/ensyu1_1/ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/ensyu1_1.java
@@ -1,9 +1,23 @@
-package ensyu1_1;
 
+package ensyu1_1;
+/*
+ * クラス名：ensyu1_1
+ * 概要：文字の出力
+ * 作成者：関口駿太郎
+ * 作成日：2023年4月4日
+ */
 public class ensyu1_1 {
-	
+	/*関数名：main
+	 * メソッドの説明：セミコロンが欠如した場合の動作確認用
+	 * パラメータの説明：なし
+	 * 返り値：なし
+	 * 作成者：関口駿太郎
+	 * 作成日：2023年4月4日
+	 */
 	public static void main(String[] args) {
+	//初めてのJavaプログラムと表示されるようにしている。	
 		System.out.println("初めてのJavaプログラム")
+	//画面に出力しています。と表示されるようにしている。
 		System.out.println("画面に出力しています。")
 	}
 
