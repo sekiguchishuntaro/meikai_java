43ee3791f0174a9cc179f695b17b32fbc751517e
ddt_sekiguchi <a@abc.com>
2023-04-05 16:23:34 +0900

    ensyu1_1のインデントの修正を行いました。ご確認お願いします。

--- ensyu1_1/src/ensyu1_1/ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/ensyu1_1.java
@@ -1,10 +1,30 @@
 package ensyu1_1;
 
+/*
+ * クラス名：ensyu1_1
+ * 概要：演習1-1クラス
+ * 作成者：関口駿太郎
+ * 作成日：2023.04.04
+ */
 public class ensyu1_1 {
-	
+	/*
+	 * 関数名：main
+	 * メソッドの説明：セミコロンが欠如した場合の動作確認用
+	 * パラメータの説明：なし
+	 * 返り値：なし
+	 * 作成者：関口駿太郎
+	 * 作成日：2023.04.04
+	 */
+
 	public static void main(String[] args) {
-		System.out.println("初めてのJavaプログラム")
-		System.out.println("画面に出力しています。")
+		/*
+		 * 初めてのJavaプログラムと表示させる
+		 */
+		System.out.println("初めてのJavaプログラム");
+		/*
+		 * 画面に出力しています。と表示させる
+		 */
+		System.out.println("画面に出力しています。");
 	}
 
 }
