c25e321b6f5e1df517ca8377117b793a7582f536
ddt_sekiguchi <a@abc.com>
2023-04-05 17:24:33 +0900

    ensyu1_2です。コメントを追加しました。

--- ensyu1_2/src/ensyu1_2/ensyu1_2.java
+++ ensyu1_2/src/ensyu1_2/ensyu1_2.java
@@ -1,7 +1,24 @@
 package ensyu1_2;
 
+/*
+* クラス名：ensyu1_2
+* 概要：演習1_2クラス
+* 作成者：関口駿太郎
+* 作成日：2023.04.05
+*/
 public class ensyu1_2 {
+	/*
+	* 関数名：main
+	* 概要：各行に一文字ずつ名前を表示するプログラム
+	* 引数：なし
+	* 戻り値：なし
+	* 作成者：関口駿太郎
+	* 作成日：2023.04.05
+	*/
 	public static void main(String[] args) {
+		
+		//各行に一文字ずつ名前を表示する
+
 		System.out.println("関\n口\n駿\n太\n郎");
 	}
 
