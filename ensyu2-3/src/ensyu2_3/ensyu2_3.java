4a31e5fc9f52a2a50d6f564cc5a164c04e5b8124
ddt_sekiguchi <a@abc.com>
2023-04-06 15:58:12 +0900

    ensyu2_3です。ご確認お願いします。

---
+++ ensyu2-3/src/ensyu2_3/ensyu2_3.java
@@ -0,0 +1,37 @@
+package ensyu2_3;
+
+import java.util.Scanner;
+/*
+ * クラス名：ensyu2_3
+ * 概要：演習2_3クラス
+ * 作成者：関口駿太郎
+ * 作成日：2023.04.06
+ */
+public class ensyu2_3 {
+	/*
+	 * 関数名：main
+	 * メソッドの説明：キーボードから読み込んだ整数値をそのまま反復して表示するプログラム
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：関口駿太郎
+	 * 作成日：2023.04.06
+	 */
+	public static void main(String[]args) {
+		//キーボードで入力したものを促す
+		Scanner stansardInput = new Scanner(System.in);
+		
+		//整数値の入力を促す
+		System.out.print("整数値:");
+		
+		//firstNumberに整数値を読み込む
+		int firstNumber = stdin.nextInt();
+		
+		//（と入力しましたね）という文字を出力する
+		System.out.println(firstNumber + "と入力しましたね。");
+	   
+	   
+	   
+	}
+	
+
+}
