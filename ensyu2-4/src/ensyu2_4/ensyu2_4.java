1a75724d006e2d1521070e8cdab7c7d4d73aae0e
ddt_sekiguchi <a@abc.com>
2023-04-06 16:24:11 +0900

    ensyu2_4です。ご確認お願いします。

---
+++ ensyu2-4/src/ensyu2_4/ensyu2_4.java
@@ -0,0 +1,36 @@
+package ensyu2_4;
+
+import java.util.Scanner;
+/*
+ * クラス名：essyu2_4
+ * 概要：演習2_4クラス
+ * 作成者：関口駿太郎
+ * 作成日：2023.04.06
+ */
+
+public class ensyu2_4 {
+	/*
+	 * 関数名：main
+	 * メソッドの説明：キーボードから読み込んだ整数値に１０を加えた値と１０を減じた値を出力するプログラム
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：関口駿太郎
+	 * 作成日：2023.04.06
+	 */
+	public static void main(String[] args) {
+
+		//キーボードで入力したものを促す
+		Scanner standardInput = new Scanner(System.in);
+
+		//整数値の入力を促す
+		System.out.print("整数値:");
+
+		//firstNumberに整数値を読み込む
+		int firstNumber = standardInput.nextInt();
+
+		//10を加えた値が出力される
+		System.out.println("10を加えた値は" + (firstNumber + 10) + "です。");
+		//10を減じた値が出力される
+		System.out.println("10を減じた値は" + (firstNumber - 10) + "です。");
+	}
+		}
