ad74fab435f81bf8e39d966d404e2d0fb9b6f88e
ddt_sekiguchi <a@abc.com>
2023-04-10 17:40:33 +0900

    修正しました。

--- ensyu3_6/src/ensyu3_6/ensyu3_6.java
+++ ensyu3_6/src/ensyu3_6/ensyu3_6.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 */
 
 public class ensyu3_6 {
-	
+
 	/*
 	 * 関数名：main
 	 * メソッドの説明：10の倍数かどうか、正ではない値を入力した場合
@@ -21,38 +21,36 @@ public class ensyu3_6 {
 	 * 作成日：2023.04.10
 	 */
 	public static void main(String[] args) {
-		
+
 		//キーボードで入力したものを表示させるために促す
 		Scanner standardInput = new Scanner(System.in);
-		
+
 		//整数値を出力する
 		System.out.print("整数値:");
-		
+
 		//キーボードで打ちこんだ整数をfirstNumberに読み込む
 		int firstNumber = standardInput.nextInt();
-		
+
 		//firstNumberが0より大きい場合
 		if (firstNumber > 0)
-			
+
 			//firstNumberを10で割った時、割り切れるか割り切れないかを判断
 			if (firstNumber % 10 == 0)
-				
+
 				//割り切れた場合（その値は10の倍数です。）と出力する
-				System.out.println ("その値は10の倍数です。");
-		//割り切れない場合
+				System.out.println("その値は10の倍数です。");
+			//割り切れない場合
 			else
-				
+
 				//割り切れない場合は、（その値は10の倍数ではありません。）と出力する
-				System.out.println ("その値は10の倍数ではありません。");
-		
+				System.out.println("その値は10の倍数ではありません。");
+
 		//正以外の値が入力された場合
 		else
-			
+
 			//正以外の値が入力された場合、（正でない値が入力されました。）と出力する
-			System.out.println ("正でない値が入力されました。");
-				
-				
+			System.out.println("正でない値が入力されました。");
+
 	}
-	
 
 }
