4d5d7c36a27b4ec13693b39b9ccac620c2f4f046
ddt_sekiguchi <a@abc.com>
2023-04-20 18:51:24 +0900

    ensyu4_11です。

--- ensyu4_11/src/ensyu4_11/Ensyu4_11.java
+++ ensyu4_11/src/ensyu4_11/Ensyu4_11.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 
 public class Ensyu4_11 {
-	
+
 	/*
 	 * 関数名：main
 	 * メソッドの説明：正の整数値を0までカウントダウンするプログラム
@@ -24,24 +24,23 @@ public class Ensyu4_11 {
 		//キーボードで入力したものを表示させるために,新しく取り込む準備
 		Scanner standardInput = new Scanner(System.in);
 		//（カウントダウンします。）と出力する
-		System.out.println ("カウントダウンします。");
+		System.out.println("カウントダウンします。");
 		//firstNumberを0で初期化する
 		int firstNumber = 0;
 		//doからwhileまで一度実行
 		do {
 			//（正の整数値：）を出力する
-			System.out.print ("正の整数値：");
+			System.out.print("正の整数値：");
 			//firstNumberに入る値を取り込む
 			firstNumber = standardInput.nextInt();
-			}
-			//firstnumberの値が0より大きいか等しい場合
-			while (firstNumber <= 0);
-			//firstNumberが0より大きいか等しい場合。繰り返されるたびfirstNumberの値が1ずつ減少する
-			for ( ;firstNumber >= 0;firstNumber --)
-				//(firNumber)を出力する
-				System.out.println (firstNumber);
-			
-		
+		}
+		//firstnumberの値が0より大きいか等しい場合
+		while (firstNumber <= 0);
+		//firstNumberが0より大きいか等しい場合。繰り返されるたびfirstNumberの値が1ずつ減少する
+		for (; firstNumber >= 0; firstNumber--)
+			//(firNumber)を出力する
+			System.out.println(firstNumber);
+
 	}
 
 }
