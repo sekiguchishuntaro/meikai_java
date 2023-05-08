af8661ca906464ad7c7554df83edf2565590d8f0
ddt_sekiguchi <a@abc.com>
2023-05-08 18:40:59 +0900

    ensyu5_3です。

---
+++ ensyu5_3/src/ensyu5_3/Ensyu5_3.java
@@ -0,0 +1,42 @@
+package ensyu5_3;
+
+//キーボードから値や文字を入力するための準備
+import java.util.Scanner;
+
+/*
+* クラス名：ensyu5_3
+* 概要：演習5_3クラス
+* 作成者：関口駿太郎
+* 作成日：2023.04.20
+*/
+
+public class Ensyu5_3 {
+	
+	/*
+	 * 関数名：main
+	 * メソッドの説明：論理型の変数にtrueやfalseを代入して、その値を表示するプログラム
+	 * 引数：なし
+	 * 戻り値：なし
+	 * 作成者：関口駿太郎
+	 * 作成日：2023.04.20
+	 */
+	public static void main(String[] args) {
+		//キーボードで入力したものを表示させるために,新しく取り込む準備
+		Scanner standardInput = new Scanner(System.in);
+		//firstNumberの値にtrueを代入
+		boolean firstNumber =true;
+		//secondNumberの値にfalseを代入
+		boolean secondNumber =false;
+		//("firstNumber =" + firstNumber )と出力する
+		System.out.println ("firstNumber =" + firstNumber );
+		//("secondnumber =" + secondNumber)と出力する
+		System.out.println ("secondnumber =" + secondNumber);
+		
+		
+		
+		
+		
+		
+
+	}
+}
